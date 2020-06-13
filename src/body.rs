586ae8bb7bfa0f0ba735df56c07bfe3093b442e9
Fraser Balch <farseer.ulwithy@gmail.com>
2020-06-12 23:52:22 -0600

    reimplemented orbit calculation and added tests

--- src/body.rs
+++ src/body.rs
@@ -8,11 +8,9 @@ pub struct Body {
     pub name: Component<Self, String>,
     pub mass: Component<Self, Mass>,
     pub radius: Component<Self, Length>,
+    pub orbit: Component<Self, Orbit>,
 
     pub star: Component<Self, Id<Star>>,
-
-    pub planets: HashMap<Id<Self>, Orbit>,
-    pub moons: HashMap<Id<Self>, MoonOrbit>,
 }
 
 impl Arena for Body {
@@ -31,16 +29,30 @@ impl Body {
 
         self.star.insert(id, link.star);
 
-        if let Some(parent) = link.parent {
-            self.moons.insert(id, MoonOrbit {
-                parent,
-                orbit: row.orbit,
-            });
-        } else {
-            self.planets.insert(id, row.orbit);
-        }
+        let orbit = self.get_orbit(link.parent, row.orbit);
+        self.orbit.insert(id, orbit);
 
         id
+    }
+
+    fn get_orbit(&self, parent: Option<Id<Body>>, params: OrbitParams) -> Orbit {
+        let parent = parent
+            .and_then(|parent| self.orbit.get(parent))
+            .map(|orbit| {
+                assert!(orbit.parent.is_none());
+                orbit.params
+            });
+
+        Orbit {
+            params,
+            parent,
+        }
+    }
+
+    pub fn get_position(&self, id: Id<Body>, time: Time) -> Option<Position> {
+        self.orbit
+            .get(id)
+            .map(|orbit| orbit.calculate_position(time))
     }
 }
 
@@ -49,7 +61,7 @@ pub struct BodyRow {
     pub name: String,
     pub mass: Mass,
     pub radius: Length,
-    pub orbit: Orbit,
+    pub orbit: OrbitParams,
 }
 
 #[derive(Debug)]
@@ -58,15 +70,106 @@ pub struct BodyLinks {
     parent: Option<Id<Body>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Orbit {
+    pub params: OrbitParams,
+    pub parent: Option<OrbitParams>,
+}
+
+impl Orbit {
+    pub fn calculate_position(&self, time: Time) -> Position {
+        if let Some(parent) = self.parent {
+            parent.calculate_position(time) + self.params.calculate_position(time)
+        } else {
+            self.params.calculate_position(time)
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct OrbitParams {
     pub radius: Length,
     pub period: Time,
     pub offset: Angle,
 }
 
-#[derive(Debug)]
-pub struct MoonOrbit {
-    pub parent: Id<Body>,
-    pub orbit: Orbit,
+impl OrbitParams {
+    pub fn calculate_position(&self, time: Time) -> Position {
+        let angle = self.get_angle(time);
+        Position::from_angle_and_radius(angle, self.radius)
+    }
+
+    pub fn get_angle(&self, time: Time) -> Angle {
+        Angle::in_rad(time / self.period * Self::NEG_TWO_PI) - self.offset
+    }
+
+    const NEG_TWO_PI: f64 = std::f64::consts::PI * -2.0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn orbit_test_at_time_zero() {
+        let (bodies, id) = get_one_planet();
+
+        let zero = bodies.get_position(id, Time::in_s(0.0)).unwrap();
+
+        assert_eq!(Position::in_m(0.0, 1000.0), zero);
+    }
+
+    #[test]
+    fn orbit_test_at_quarter_orbit() {
+        let (bodies, id) = get_one_planet();
+
+        let quarter = bodies.get_position(id, Time::in_s(15.0)).unwrap();
+
+        assert_eq!(Length::in_m(-1000.0), quarter.x);
+    }
+
+    #[test]
+    fn orbit_test_at_half_orbit() {
+        let (bodies, id) = get_one_planet();
+
+        let half = bodies.get_position(id, Time::in_s(30.0)).unwrap();
+
+        assert_eq!(Length::in_m(-1000.0), half.y);
+        assert!(Length::in_m(0.001) > half.x && half.x > Length::in_m(-0.001));
+    }
+
+    #[test]
+    fn orbit_test_at_three_quarter_orbit() {
+        let (bodies, id) = get_one_planet();
+
+        let three_quarters = bodies.get_position(id, Time::in_s(45.0)).unwrap();
+
+        assert_eq!(Length::in_m(1000.0), three_quarters.x);
+    }
+
+    fn get_one_planet() -> (Body, Id<Body>) {
+        let mut body = Body::default();
+
+        let star = Allocator::<Star>::default().create();
+
+        let planet = BodyRow {
+            name: "Planet".to_string(),
+            mass: Mass::in_kg(100.0),
+            radius: Length::in_m(1.0),
+            orbit: OrbitParams {
+                radius: Length::in_m(1000.0),
+                period: Time::in_s(60.0),
+                offset: Angle::default(),
+            }
+        };
+
+        let links = BodyLinks {
+            star,
+            parent: None
+        };
+
+        let id = body.create(planet, links);
+
+        (body, id)
+    }
 }
--- src/components.rs
+++ src/components.rs
@@ -1,3 +1,5 @@
+use std::ops::*;
+
 #[derive(Debug, Default, Copy, Clone, PartialEq, PartialOrd)]
 pub struct Mass(f64);
 
@@ -13,6 +15,14 @@ pub struct Length(f64);
 impl Length {
     pub fn in_m(meters: f64) -> Self {
         Self(meters)
+    }
+}
+
+impl Add for Length {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::in_m(self.0 + rhs.0)
     }
 }
 
@@ -34,11 +44,69 @@ impl Time {
     }
 }
 
+impl Div for Time {
+    type Output = f64;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        self.0 / rhs.0
+    }
+}
+
 #[derive(Debug, Default, Copy, Clone, PartialEq, PartialOrd)]
 pub struct Angle(f64);
 
 impl Angle {
     pub fn in_rad(radians: f64) -> Self {
         Self(radians)
+    }
+}
+
+impl Add for Angle {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::in_rad(self.0 + rhs.0)
+    }
+}
+
+impl Sub for Angle {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::in_rad(self.0 - rhs.0)
+    }
+}
+
+#[derive(Debug, Default, Copy, Clone, PartialEq)]
+pub struct Position {
+    pub x: Length,
+    pub y: Length
+}
+
+impl Position {
+    pub fn in_m(x: f64, y: f64) -> Self {
+        Self { x: Length::in_m(x), y: Length::in_m(y) }
+    }
+
+    /// Returns the position given an angle and a radius
+    ///
+    ///  # Arguments
+    ///
+    /// * `angle` - An angle as measured clockwise from the positive y-axis
+    /// * `length` - The length of the resulting vector
+    pub fn from_angle_and_radius(angle: Angle, length: Length) -> Self {
+        let x = angle.0.sin() * length.0;
+        let y = angle.0.cos() * length.0;
+        Self::in_m(x, y)
+    }
+}
+
+impl Add for Position {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let x = self.x + rhs.x;
+        let y = self.y + rhs.y;
+        Self { x, y }
     }
 }
