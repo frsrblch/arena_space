bfbd129df63e191360115eb888118bf5844c8cb5
Fraser Balch <farseer.ulwithy@gmail.com>
2020-06-17 12:24:13 -0600

    fleshed out government arena a little

--- src/government.rs
+++ src/government.rs
@@ -5,6 +5,25 @@ pub struct Government {
     pub alloc: Allocator<Self>,
 
     pub name: Component<Self, String>,
+
+    pub population: Component<Self, Population>,
 }
 
 dynamic_arena!(Government, u16);
+
+impl Government {
+    pub fn create(&mut self, row: GovernmentRow, links: ()) -> Id<Self> {
+        let id = self.alloc.create();
+
+        self.name.insert(id, row.name);
+
+        self.population.insert(id, Population::default());
+
+        id.id
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct GovernmentRow {
+    pub name: String,
+}
