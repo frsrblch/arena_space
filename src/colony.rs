ad7152bd97fe80b6e17413d7f0a448f8d5eea10d
Fraser Balch <farseer.ulwithy@gmail.com>
2020-07-13 23:05:53 -0600

    refactoring

--- src/colony.rs
+++ src/colony.rs
@@ -89,6 +89,8 @@ mod population {
         //
         //                      land usage: https://ourworldindata.org/land-use
         pub fn update_population(&mut self, bodies: &Bodies) {
+            // TODO: body sum population from colonies so that multiple colonies on one planet limit growth (e.g., Earth)
+
             let year_fraction = System::ColonyPopulation.get_interval_as_year_fraction();
 
             self.population.iter_mut()
