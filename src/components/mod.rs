8dca008326399ee286d35d0aedf7c23ceab16a86
Fraser Balch <farseer.ulwithy@gmail.com>
2020-08-08 23:48:43 -0600

    added Credits and related types

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -243,7 +243,30 @@ impl Display for TonsPerDay {
 scalar_div!(Mass, DurationFloat, MassRate);
 
 scalar!(MassRatePerPerson, kg_per_person_second, kg_per_s_person);
+pub type Productivity = MassRatePerPerson;
 scalar_div!(MassRate, Population, MassRatePerPerson);
+
+scalar!(Credits, credits, credits);
+
+scalar!(CreditRate, credits_per_second, credits_per_s);
+scalar_div!(Credits, DurationFloat, CreditRate);
+
+scalar!(CreditsPerPerson, credits_per_person, credits_per_person);
+scalar_div!(Credits, Population, CreditsPerPerson);
+
+scalar!(CreditsPerSecondPerPerson, credits_per_second_person, credits_per_s_person);
+pub type Wage = CreditsPerSecondPerPerson;
+scalar_div!(CreditRate, Population, CreditsPerSecondPerPerson);
+scalar_div!(CreditsPerPerson, DurationFloat, CreditsPerSecondPerPerson);
+
+scalar!(CreditsPerKilogram, credits_per_kilogram, credits_per_kg);
+pub type Price = CreditsPerKilogram;
+scalar_div!(CreditsPerSecondPerPerson, MassRatePerPerson, CreditsPerKilogram);
+
+// kg/s/person      - productivity
+// credits/s/person - wage
+// credits/kg       - price
+
 
 #[derive(Debug, Default, Copy, Clone, PartialOrd, PartialEq)]
 pub struct Fraction(f64);
