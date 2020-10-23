f85d93b4ad97780632475db52166aa426191f08f
Fraser Balch <farseer.ulwithy@gmail.com>
2020-10-22 23:25:04 -0600

    Add const Speed::C

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -115,6 +115,10 @@ vector_and_scalar!(Velocity, Speed, meters_per_second, in_m_per_s);
 
 scalar_div!(Length, DurationFloat, Speed);
 
+impl Speed {
+    pub const C: Speed = Speed::in_m_per_s(299792458.0);
+}
+
 impl Distance {
     /// Returns the position vector given an angle and a radius
     ///
