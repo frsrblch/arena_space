57d00092b5e382e6fc2d10221eaca5be08555b81
Fraser Balch <farseer.ulwithy@gmail.com>
2020-12-02 11:17:07 -0700

    Refactored freighter test as example

--- examples/freighters.rs
+++ examples/freighters.rs
@@ -1,13 +1,69 @@
-use super::*;
-use crate::body::examples::{earth, luna};
-use crate::body::Planet;
-use crate::colony::economy::ProductionUnit;
-use crate::colony::ColonyLinks;
-use crate::star::examples::sol;
-use crate::star::StarSystem;
-use crate::system_state::SystemState;
+use arena_space::body::examples::{earth, luna};
+use arena_space::body::Planet;
+use arena_space::colony::economy::ProductionUnit;
+use arena_space::colony::*;
+use arena_space::components::*;
+use arena_space::ships::drives::Drive;
+use arena_space::ships::freighter_assignment::Assignment;
+use arena_space::ships::*;
+use arena_space::star::examples::sol;
+use arena_space::star::*;
+use arena_space::system_state::*;
+use gen_id::*;
 use rand::SeedableRng;
 use wyhash::WyRng;
+
+fn main() {
+    let TestState {
+        mut state,
+        city_colony,
+        farm_colony,
+    } = get_test_state();
+
+    let rng = &mut WyRng::seed_from_u64(0);
+
+    for _ in 0..20 {
+        let f = state.state.freighter.create(
+            get_random_freighter(rng),
+            FreighterLinks {
+                location: farm_colony,
+            },
+        );
+
+        {
+            let f = Valid::assert(f);
+            let assignment = &mut state.state.freighter.assignment;
+            assignment.insert(f, Some(Assignment::Route(city_colony, farm_colony)));
+        }
+    }
+
+    state.update_by(2.0 * YR);
+
+    for _ in 0..6 {
+        state.update_by(30.0 * DAY);
+
+        state.state.time.print();
+        state.state.colony.print();
+        println!();
+    }
+
+    println!("done: {}\n", &state.state.time);
+
+    let colony = &state.state.colony;
+    let get_name = |id: Id<Colony>| colony.name.get(id);
+    let get_satiation = |id: Id<Colony>| colony.people.satiation.get(id).value();
+
+    println!(
+        "{}: {:.2}",
+        get_name(city_colony),
+        get_satiation(city_colony)
+    );
+    println!(
+        "{}: {:.2}",
+        get_name(farm_colony),
+        get_satiation(farm_colony)
+    );
+}
 
 struct TestState {
     state: SystemState,
@@ -79,62 +135,4 @@ fn get_random_freighter(rng: &mut impl rand::Rng) -> Freighter {
         loading_rate: rng.gen_range(1.0, 2.5) * TON / MIN,
         drive: Drive::Warp(rng.gen_range(40.0, 60.0) * KM / S),
     }
-}
-
-// TODO this isn't a test anymore, move it to examples
-#[allow(unused_variables)]
-#[test]
-#[ignore]
-fn idle_freighter_without_assignment_remains_idle() {
-    let TestState {
-        mut state,
-        city_colony,
-        farm_colony,
-    } = get_test_state();
-
-    let rng = &mut WyRng::seed_from_u64(0);
-
-    for _ in 0..20 {
-        let f = state.state.freighter.create(
-            get_random_freighter(rng),
-            FreighterLinks {
-                location: farm_colony,
-            },
-        );
-
-        {
-            let f = Valid::assert(f);
-            let assignment = &mut state.state.freighter.assignment;
-            assignment.insert(f, Some(Assignment::Route(city_colony, farm_colony)));
-        }
-    }
-
-    state.update_by(2.0 * YR);
-
-    for _ in 0..6 {
-        state.update_by(30.0 * DAY);
-
-        state.state.time.print();
-        state.state.colony.print();
-        println!();
-    }
-
-    println!("done: {}\n", &state.state.time);
-
-    let colony = &state.state.colony;
-    let get_name = |id: Id<Colony>| colony.name.get(id);
-    let get_satiation = |id: Id<Colony>| colony.people.satiation.get(id).value();
-
-    println!(
-        "{}: {:.2}",
-        get_name(city_colony),
-        get_satiation(city_colony)
-    );
-    println!(
-        "{}: {:.2}",
-        get_name(farm_colony),
-        get_satiation(farm_colony)
-    );
-
-    panic!();
 }
